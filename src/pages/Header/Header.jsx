358c95879e2be996537c0b8e537dbbbc297cdd71
TaHyc <stas.rogal89@gmail.com>
2023-03-31 16:28:27 +0300

    adaptiv

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -17,22 +17,26 @@ return  (
 	<>
 	<div className={cl.header}>
 		<div className={cl.up}>
-		<Link to=""><img className={cl.logo} src={title}/></Link>
-		<div className={cl.work}>Пн-Сб: c 10:00 до 20:00<br></br>Выходной: воскресенье</div>	
-		<img className={cl.kub} src={kub}/><div className={cl.zakaz}>Заказать 3D-макет дома</div>
-		<div className={cl.phone}><img className={cl.phoneLogo} src={phone}/>
-		<div className={cl.phoneVert}>+7 (915) 168-55-50<br></br>
-		<div className={cl.vtw}>
-	<a href='https://t.me/AquaTechnology_bot'><img className={cl.cont} src={viber}/></a>
-	<a href='https://t.me/AquaTechnology_bot'><img className={cl.cont}src={tg}/></a>
-	<a href='https://t.me/AquaTechnology_bot'><img className={cl.cont}src={ws}/></a></div></div></div>
-
+			<Link to=""><img className={cl.logo} src={title}/></Link>
+			<div className={cl.work}>Пн-Сб: c 10:00 до 20:00<br></br>Выходной: воскресенье</div>	
+				<div className={cl.zakaz}>
+					<img className={cl.kub} src={kub}/>
+					<div className={cl.treeD}>Заказать 3D-макет дома</div>
+				</div>
+			<div className={cl.phone}>
+				<img className={cl.phoneLogo} src={phone}/>
+				<div className={cl.phoneVert}>+7 (915) 168-55-50<br></br>
+			<div className={cl.vtw}>
+				<a href='https://t.me/AquaTechnology_bot'><img className={cl.cont} src={viber}/></a>
+				<a href='https://t.me/AquaTechnology_bot'><img className={cl.cont}src={tg}/></a>
+				<a href='https://t.me/AquaTechnology_bot'><img className={cl.cont}src={ws}/></a>
+			</div>
+			</div>
+			</div>
 			<button className={cl.button}>Заказать звонок</button>
-
 		</div>	
-		<Menu />
+			<Menu />
 	</div>
-
 
 	</>
 )
